0d94d1a430149ca348d399e2f6cb58c4974f0537
abj1389 <abj_13@hotmail.com>
2023-05-19 19:36:03 +0200

    added user model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const validator = require("validator");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [3, "El nombre debe tener al menos 3 caractéres y máximo 100 caracteres"],
+      maxLength: [100, "El nombre debe tener al menos 3 caractéres y máximo 100 caracteres"],
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Email incorrecto",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: validator.isStrongPassword({ minSymbols: 0 }),
+        message: "La contraseña debe tener como mínimo 8 caractéres, una mayúscula, una minúscula y un número",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
